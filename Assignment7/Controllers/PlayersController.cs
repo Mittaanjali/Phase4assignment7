9f9222843f2056fc21b16d78d49f1a9ca5e001c3
Mittaanjali <mitta.anjali@mphasis.com>
2024-01-17 16:52:50 +0530

    Assignment7

---
+++ Assignment7/Controllers/PlayersController.cs
@@ -0,0 +1,30 @@
+﻿using Assignment7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class PlayersController : Controller
+    {
+        static List<Player> players = new List<Player>()
+        {
+            new Player() { PId = 1, PName = "Dhoni", PCountry = "India", PType = "All-rounder" },
+            new Player() { PId = 2, PName = "Rahul", PCountry = "Australia", PType = "Wicketkeeper" },
+            new Player() { PId = 3, PName = "Virat", PCountry = "India", PType = "Batsman" },
+        };
+        public IActionResult Index()
+        {
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Player player)
+        {
+            players.Add(player);
+            return RedirectToAction("Index");
+        }
+    }
+}
